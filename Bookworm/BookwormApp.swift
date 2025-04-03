6304c9c244c7a024b26f83e8012957057e66bf30
developerkamol <developerkamol@gmail.com>
2025-04-03 22:00:57 +0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Kamol Madaminov on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
