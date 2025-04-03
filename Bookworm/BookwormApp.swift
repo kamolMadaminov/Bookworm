fffb232f0a335c92ecfca87cb23e411dfee79f5d
developerkamol <developerkamol@gmail.com>
2025-04-03 22:34:26 +0500

    Initial commit

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Kamol Madaminov on 03/04/25.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Student.self)
     }
 }
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,17 +5,32 @@
 //  Created by Kamol Madaminov on 03/04/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var students: [Student]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(students){ student in
+                    Text(student.name)
+            }
+            .navigationTitle("Classroom")
+            .toolbar {
+                Button("Add"){
+                    let firstNames = ["Kamol", "Nilufar", "Sultan", "Nilufar", "Sultan"]
+                    let lastNames = ["Madaminov", "Zakirova", "Khuslenov", "Zakirova", "Khuslenov"]
+                    
+                    let chosenFirstName = firstNames.randomElement() ?? "Unknown"
+                    let chosenLastName = lastNames.randomElement() ?? "Unknown"
+                    
+                    let student = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
+                    modelContext.insert(student)
+                }
+            }
         }
-        .padding()
     }
 }
 
